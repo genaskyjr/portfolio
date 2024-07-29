306fe53b2fb60bae00161fde1d5e22f4125e9c51
robotvpn <38845773+genaskyjr@users.noreply.github.com>
2024-07-30 02:00:00 +0800

    added tect stacks per projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -2,6 +2,15 @@ import InternImage from './assets/intern/front.jpg'
 import ThesisImage from './assets/thesis/front.jpg'
 import WebSysImage from './assets/websys/front.jpg'
 
+import Html from './assets/Html 5.png'
+import Css from './assets/CSS3.png'
+import JavaScript from './assets/JavaScript.png'
+import BootStrap from './assets/Bootstrap.png'
+import Php from './assets/PHP Logo.png'
+import MySQL from './assets/MySQL Logo.png'
+import VS from './assets/Visual Studio Code 2019.png' 
+
+
 import { IoIosArrowDropdown } from "react-icons/io";
 
 import GitHub from './assets/GitHub.png'
@@ -20,6 +29,15 @@ function Projects(){
 
                 <div className='bg-[#54608e] rounded-lg font-roboto font-semibold'>
                     <div className='h-64 relative'>
+                        <div className='absolute flex left-1 bottom-1 bg-[#00002ea5] bg-opacity-90 rounded-sm p-1'>
+                            <img className='w-5' src={Html}></img> 
+                            <img className='w-5' src={Css}></img> 
+                            <img className='w-5' src={JavaScript}></img> 
+                            <img className='w-5' src={BootStrap}></img> 
+                            <img className='w-5' src={Php}></img> 
+                            <img className='w-5' src={MySQL}></img> 
+                            <img className='w-5' src={VS}></img> 
+                        </div>
                         <span className='absolute bg-[#4e5b8c] pl-3 pr-3 pt-2 pb-2 rounded-md 
                         h-max w-max mx-auto left-0 right-0 top-0 bottom-0 my-auto font-semibold font-roboto'>View Images</span>
                         <img className=' rounded-t-lg object-cover h-full w-full' src={InternImage}></img>
@@ -34,9 +52,21 @@ function Projects(){
                 </div>
 
                 <div className='bg-[#54608e] rounded-lg font-roboto font-semibold'>
-                    <div className=' h-64 z'>
-                        <img className='rounded-t-lg object-cover h-full w-full' src={ThesisImage}></img>
+                    <div className='h-64 relative'>
+                        <div className='absolute flex left-1 bottom-1 bg-[#00002ea5] bg-opacity-90 rounded-sm p-1'>
+                            <img className='w-5' src={Html}></img> 
+                            <img className='w-5' src={Css}></img> 
+                            <img className='w-5' src={JavaScript}></img> 
+                            <img className='w-5' src={BootStrap}></img> 
+                            <img className='w-5' src={Php}></img> 
+                            <img className='w-5' src={MySQL}></img> 
+                            <img className='w-5' src={VS}></img> 
+                        </div>
+                        <span className='absolute bg-[#4e5b8c] pl-3 pr-3 pt-2 pb-2 rounded-md 
+                        h-max w-max mx-auto left-0 right-0 top-0 bottom-0 my-auto font-semibold font-roboto'>View Images</span>
+                        <img className=' rounded-t-lg object-cover h-full w-full' src={InternImage}></img>
                     </div>  
+                    
                     <div className=' pt-2 pb-2'>
                         <p className='text-base text-center'>Noah's Ark Dog & Cat Shelter</p>
                         <p className='text-sm text-center'>Thesis - Company</p>
@@ -45,10 +75,21 @@ function Projects(){
                     </div>  
                 </div>
 
-                                <div className='bg-[#54608e] rounded-lg font-roboto font-semibold'>
-                    <div className=' h-64  '>
-                        <img className='rounded-t-lg object-cover h-full w-full' src={WebSysImage}></img>
-                    </div>  
+                <div className='bg-[#54608e] rounded-lg font-roboto font-semibold'>
+                    <div className='h-64 relative'>
+                        <div className='absolute flex left-1 bottom-1 bg-[#00002ea5] bg-opacity-90 rounded-sm p-1'>
+                            <img className='w-5' src={Html}></img> 
+                            <img className='w-5' src={Css}></img> 
+                            <img className='w-5' src={JavaScript}></img> 
+                            <img className='w-5' src={BootStrap}></img> 
+                            <img className='w-5' src={Php}></img> 
+                            <img className='w-5' src={MySQL}></img> 
+                            <img className='w-5' src={VS}></img> 
+                        </div>
+                        <span className='absolute bg-[#4e5b8c] pl-3 pr-3 pt-2 pb-2 rounded-md 
+                        h-max w-max mx-auto left-0 right-0 top-0 bottom-0 my-auto font-semibold font-roboto'>View Images</span>
+                        <img className=' rounded-t-lg object-cover h-full w-full' src={InternImage}></img>
+                    </div>    
                     <div className=' pt-2 pb-2'>
                         <p className='text-base text-center'>Document Approval System</p>
                         <p className='text-sm text-center'>WebSys - Company</p>
