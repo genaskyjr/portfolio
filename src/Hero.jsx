b0d7aa7a9abe35f7af9ee5215848e76696ed6732
genaskyjr <genaskypinlac0@gmail.com>
2025-07-29 17:29:16 +0400

    updated

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,7 +4,9 @@ import { IoCall } from "react-icons/io5";
 import GmailImage from  './assets/gmail-icon.png';
 import MessagerImage from './assets/messager-icon.png';
 
-import Resume from './assets/web-developer-genaskyjr-08.pdf';
+import WhatsAppImage from './assets/WhatsApp.png';
+
+import Resume from './assets/GenaskyPinlac_FullStackWebDeveloper.pdf';
 
 import { FaHeart } from "react-icons/fa";
 
@@ -23,7 +25,7 @@ function Hero(){
                 <p className='animate-bounce mx-auto  animate-infinite font-outfit font-bold relative -z-10 text-2xl
                 sm:text-3xl md:text-4xl lg:text-5xl'>Hi There!</p>
                 <p className='font-outfit font-bold text-2xl 
-                sm:text-3xl md:text-4xl lg:text-5xl'>I'am <span className='pl-1 text-[#76BFE8] font-outfit font-bold text-3xl
+                sm:text-3xl md:text-4xl lg:text-5xl'>I'm <span className='pl-1 text-[#76BFE8] font-outfit font-bold text-3xl
                 sm:text-4xl md:text-5xl lg:text-6xl'>Genasky Jr.</span></p>
                 <p className='text-[#5dc0f9] font-outfit font-bold mb-4  text-3xl
                 sm:text-4xl md:text-5xl lg:text-5xl'>Web Developer</p>
@@ -31,15 +33,16 @@ function Hero(){
                 <p className=' font-outfit font-bold text-xs
                 sm:text-sm md:text-base lg:text-lg'>&bull; Willing to relocate. </p>
 
-                <p className=' font-outfit font-bold text-xs
-                sm:text-sm md:text-base lg:text-lg'>&bull; Willing to train. </p>
+                {/* <p className=' font-outfit font-bold text-xs
+                sm:text-sm md:text-base lg:text-lg'>&bull; Willing to train. </p> */}
 
                 <section className='gap-2 flex justify-center xl:justify-start mt-1'>
                     
                     <a href={Resume} target='_blank'  className='hover:animate-wiggle hover:animate-infinite hover:opacity-75 font-outfit font-semibold pl-8 pr-8 pt-1 pb-1 bg-[#424C6F] rounded-lg border-x-emerald-50 border-2 hover:cursor-pointer'> <FaDownload size={17} className='inline-block align-text-middle pb-[3px]'/> RESUME</a>
-                    <a href='tel:+639287152180' className='hover:animate-wiggle hover:animate-infinite hover:opacity-75 pl-2 pr-2 rounded-lg bg-[#424C6F] border-x-emerald-50 border-2 hover:opacity-90 pt-1'><IoCall size={25} className=''/></a>
+                    <a href='tel:+971543283661' className='hover:animate-wiggle hover:animate-infinite hover:opacity-75 pl-2 pr-2 rounded-lg bg-[#424C6F] border-x-emerald-50 border-2 hover:opacity-90 pt-1'><IoCall size={25} className=''/></a>
                     <a href="mailto:genaskypinlac0@gmail.com" className='hover:animate-wiggle hover:animate-infinite hover:opacity-75 px-2 py-2 rounded-lg bg-[#424C6F] border-x-emerald-50 border-2 hover:opacity-90'><img className='w-6' src={GmailImage} alt="Gmail icon" /></a>
-                    <a href='https://m.me/genaskyjr' className='hover:animate-wiggle hover:animate-infinite hover:opacity-75 pl-2 pr-2 rounded-lg bg-[#424C6F] border-x-emerald-50 border-2 hover:opacity-90 pt-1'><img className='w-6' src={MessagerImage} alt="Messager icon" /></a>
+                    <a href='tel:+971543283661' className='hover:animate-wiggle hover:animate-infinite hover:opacity-75 pl-2 pr-2 rounded-lg bg-[#424C6F] border-x-emerald-50 border-2 hover:opacity-90 pt-1'><img className='w-6' src={WhatsAppImage} alt="WhatsApp icon" /></a>
+                
                 </section>
 
             </section>
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -14,9 +14,146 @@ import Python from './assets/Python.png'
 import { IoIosArrowDropdown } from "react-icons/io";
 
 import GitHub from './assets/GitHub.png'
+import dasVideo from './assets/videos/das.mp4'
+import PayrollVideo from './assets/videos/payroll.mp4'
+import CashFlowVideo from './assets/videos/cashflow.mp4'
+import { useState } from 'react'
+import PayrollImage from './assets/images/payroll.png'
+
+import CashFlowImage from './assets/images/cashflow.png'
 
 function Projects(){
+
+    //das
+    const [isOpen, setIsOpen] = useState(false)
+
+    //payroll
+    const [isOpen1, setIsOpen1] = useState(false)
+
+    //cashflow
+    const [isOpen2, setIsOpen2] = useState(false)
+
+
+
+
     return(
+        <>
+
+
+
+        {/* cashflow */}
+        <div className={`${isOpen2 ? 'visible' : 'hidden'}`}>
+            <div id="myModal" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+                <div class="bg-white rounded-2xl p-6 w-11/12 max-w-md mx-auto shadow-xl relative">
+                  
+                    <button onClick={()=>{setIsOpen2(false)}} class="absolute top-3 right-3 text-gray-500 hover:text-gray-800" onclick="document.getElementById('myModal').classList.add('hidden')">
+                    ✕
+                    </button>
+
+                   
+                    <h2 class="text-xl font-semibold mb-4 text-gray-600">CashFlow</h2>
+                    
+                    <div className="images overflow-x-scroll">
+                        <div className="w-full aspect-video bg-black">
+                            <video
+                                className="w-full h-full"
+                                controls
+                                autoPlay
+                            >
+                                <source src={CashFlowVideo} type="video/mp4" />
+                                Your browser does not support the video tag.
+                            </video>
+                        </div>
+                    </div>
+
+                  
+                    <div class="text-right ">
+                    <button onClick={()=>{setIsOpen2(false)}} class="bg-gray-300 text-gray-800 px-4 py-2 rounded mr-2 mt-4" onclick="document.getElementById('myModal').classList.add('hidden')">
+                        Close
+                    </button>
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        {/* payroll */}
+        <div className={`${isOpen1 ? 'visible' : 'hidden'}`}>
+            <div id="myModal" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+                <div class="bg-white rounded-2xl p-6 w-11/12 max-w-md mx-auto shadow-xl relative">
+                  
+                    <button onClick={()=>{setIsOpen1(false)}} class="absolute top-3 right-3 text-gray-500 hover:text-gray-800" onclick="document.getElementById('myModal').classList.add('hidden')">
+                    ✕
+                    </button>
+
+                   
+                    <h2 class="text-xl font-semibold mb-4 text-gray-600">Simplified Payroll</h2>
+                    
+                    <div className="images overflow-x-scroll">
+                        <div className="w-full aspect-video bg-black">
+                            <video
+                                className="w-full h-full"
+                                controls
+                                autoPlay
+                            >
+                                <source src={PayrollVideo} type="video/mp4" />
+                                Your browser does not support the video tag.
+                            </video>
+                        </div>
+                    </div>
+
+                  
+                    <div class="text-right ">
+                    <button onClick={()=>{setIsOpen1(false)}} class="bg-gray-300 text-gray-800 px-4 py-2 rounded mr-2 mt-4" onclick="document.getElementById('myModal').classList.add('hidden')">
+                        Close
+                    </button>
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        {/* das */}
+        <div className={`${isOpen ? 'visible' : 'hidden'}`}>
+            <div id="myModal" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+                <div class="bg-white rounded-2xl p-6 w-11/12 max-w-md mx-auto shadow-xl relative">
+                  
+                    <button onClick={()=>{setIsOpen(false)}} class="absolute top-3 right-3 text-gray-500 hover:text-gray-800" onclick="document.getElementById('myModal').classList.add('hidden')">
+                    ✕
+                    </button>
+
+                   
+                    <h2 class="text-xl font-semibold mb-4 text-gray-600">Document Approval System</h2>
+                    
+                    <div className="images overflow-x-scroll">
+                        <div className="w-full aspect-video bg-black">
+                            <video
+                                className="w-full h-full"
+                                controls
+                                autoPlay
+                            >
+                                <source src={dasVideo} type="video/mp4" />
+                                Your browser does not support the video tag.
+                            </video>
+                        </div>
+                    </div>
+
+                  
+                    <div class="text-right ">
+                    <button onClick={()=>{setIsOpen(false)}} class="bg-gray-300 text-gray-800 px-4 py-2 rounded mr-2 mt-4" onclick="document.getElementById('myModal').classList.add('hidden')">
+                        Close
+                    </button>
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+  
         <section id="project" className="animate-fade-down animate-once animate-duration-1000 animate-delay-100 mt-12 p-5 bg-[#424C6F] border rounded-lg border-0 mx-2 max-w-screen-lg
         sm:mx-12 md:mx-24 lg:mx-36 xl:mx-auto">
            
@@ -26,6 +163,57 @@ function Projects(){
           
             <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-x-4 gap-y-6 
              ">
+
+
+
+
+                <div className='bg-[#54608e] rounded-lg font-roboto font-semibold'>
+                    <div className='h-64 relative'>
+                        
+                        {/* <span className='absolute bg-[#4e5b8c] pl-3 pr-3 pt-2 pb-2 rounded-md 
+                        h-max w-max mx-auto left-0 right-0 top-0 bottom-0 my-auto font-semibold font-roboto'>View Images</span> */}
+                        <img className=' rounded-t-lg object-cover h-full w-full' src={CashFlowImage}></img>
+                    </div>  
+                    <div className=' pt-2 pb-2 pl-3'>
+                        <p className='text-base '>CashFlow</p>
+                        <p className='text-sm '>Myracle Innovation - Company</p>
+                        <p className='text-xs '>&bull; Responsibilty: Full Stack Developer</p>
+                        <p className='text-xs '>&bull; FE: Bootraps + custom css + AJAX + JQUERY + ApexChart</p>
+                        <p className='text-xs '>&bull; BE: PHP PHP-PDO</p>
+                        <p className='text-xs '>&bull; Database: MySql</p>
+                        <p className='text-xs '>&bull; Coding Style: OOP MVC SOLID</p>
+                        <button onClick={()=>{setIsOpen2(true)}} className='border p-1 ps-6 pe-6 rounded-sm shadow-lg mb-2 mt-2 hover:text-slate-700 hover:bg-slate-300'>View</button>
+                        
+                       
+                    </div>  
+                   
+                </div>
+
+
+
+                <div className='bg-[#54608e] rounded-lg font-roboto font-semibold'>
+                    <div className='h-64 relative'>
+                        
+                        {/* <span className='absolute bg-[#4e5b8c] pl-3 pr-3 pt-2 pb-2 rounded-md 
+                        h-max w-max mx-auto left-0 right-0 top-0 bottom-0 my-auto font-semibold font-roboto'>View Images</span> */}
+                        <img className=' rounded-t-lg object-cover h-full w-full' src={PayrollImage}></img>
+                    </div>  
+                    <div className=' pt-2 pb-2 pl-3'>
+                        <p className='text-base '>Simplified Payroll</p>
+                        <p className='text-sm '>Myracle Innovation - Company</p>
+                        <p className='text-xs '>&bull; Responsibilty: Full Stack Developer</p>
+                        <p className='text-xs '>&bull; FE: Bootraps + custom css + AJAX + JQUERY</p>
+                        <p className='text-xs '>&bull; BE: PHP PHP-PDO</p>
+                        <p className='text-xs '>&bull; Database: MySql</p>
+                        <p className='text-xs '>&bull; Coding Style: OOP MVC SOLID</p>
+                        <button onClick={()=>{setIsOpen1(true)}} className='border p-1 ps-6 pe-6 rounded-sm shadow-lg mb-2 mt-2 hover:text-slate-700 hover:bg-slate-300'>View</button>
+                        
+                       
+                    </div>  
+                   
+                </div>
+
+     
 
                 <div className='bg-[#54608e] rounded-lg font-roboto font-semibold'>
                     <div className='h-64 relative'>
@@ -46,6 +234,9 @@ function Projects(){
                         <p className='text-xs '>&bull; Domain: https://atsdasph.com</p>
                         <p className='text-xs '>&bull; Coding Style: Procedural</p>
                         <a href='https://github.com/genaskyjr/document-approval-system'><img className='pt-2 w-8' src={GitHub}></img></a>
+                        <button onClick={()=>{setIsOpen(true)}} className='border p-1 ps-6 pe-6 rounded-sm shadow-lg mb-2 mt-2 hover:text-slate-700 hover:bg-slate-300'>View</button>
+                        
+                       
                     </div>  
                    
                 </div>
@@ -113,6 +304,11 @@ function Projects(){
             </div>
 
         </section>
+        </>
+
+  
+
+        
     )
 }
 
